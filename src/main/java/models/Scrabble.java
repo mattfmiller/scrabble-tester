2faa3849fc5ab158079916f8be52fb448b9a9c6e
Matt Miller and semhar Kidane <matt@>
2018-05-22 09:54:10 -0700

    Add logic to pass first test.

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -1,7 +1,43 @@
 package models;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Scrabble {
-    public Integer calculateScore(char letterToTest){
-        return null;
+    Map<Character, Integer> characterValues = new HashMap<Character, Integer>();
+
+    public Scrabble() {
+        characterValues.put('a', 1);
+//        characterValues.put('b', 3);
+//        characterValues.put('c', 3);
+//        characterValues.put('d', 2);
+//        characterValues.put('e', 1);
+//        characterValues.put('f', 4);
+//        characterValues.put('g', 2);
+//        characterValues.put('h', 4);
+//        characterValues.put('i', 1);
+//        characterValues.put('j', 8);
+//        characterValues.put('k', 5);
+//        characterValues.put('l', 1);
+//        characterValues.put('m', 3);
+//        characterValues.put('n', 1);
+//        characterValues.put('o', 1);
+//        characterValues.put('p', 3);
+//        characterValues.put('q', 1);
+//        characterValues.put('r', 1);
+//        characterValues.put('s', 1);
+//        characterValues.put('t', 1);
+//        characterValues.put('u', 1);
+//        characterValues.put('v', 4);
+//        characterValues.put('w', 4);
+//        characterValues.put('x', 8);
+//        characterValues.put('y', 4);
+//        characterValues.put('z', 10);
+    }
+
+    public Integer calculateScore(String wordToTest) {
+       char[] wordToCharArray = wordToTest.toCharArray();
+        Character charToCharacter = wordToCharArray[0];
+        return characterValues.get(charToCharacter);
     }
 }
--- src/test/java/models/ScrabbleTest.java
+++ src/test/java/models/ScrabbleTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 
 public class ScrabbleTest {
     @Test
-    public void calculateScore_returnsScoreForSingleLetter_1() throws Exception {
+    public void calculateScore_returnsScoreForA_1() throws Exception {
         Scrabble testScrabble = new Scrabble();
         Integer expected = 1;
-        assertEquals(expected, testScrabble.calculateScore('a'));
+        assertEquals(expected, testScrabble.calculateScore("a"));
     }
 
+//    @Test
+//    public void calculateScore_returnsScoreForSingleLetter_1() throws Exception {
+//        Scrabble testScrabble = new Scrabble();
+//        Integer expected = 8;
+//        assertEquals(expected, testScrabble.calculateScore("j"));
+//    }
 }
